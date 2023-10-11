5a49c2fdc5f92537098855c9ccf43178e1918cd3
Jorgito Ramírez Galvan <jorgitoram@Jorgitos-MacBook-Air.local>
2023-10-11 11:17:44 +0200

    updated navbar script

--- resources/js/navbar_script.js
+++ resources/js/navbar_script.js
@@ -4,11 +4,7 @@ let colorChange = () => {
     let elementVisible = 150
     let scrollY = window.scrollY
 
-    if (scrollY >= elementVisible) {
-      element.classList.add("color_change")
-    } else {
-      element.classList.remove("color_change")
-    }
+    scrollY >= elementVisible ? element.classList.add("color_change") : element.classList.remove("color_change")
 }
 
 window.addEventListener("scroll", colorChange)
